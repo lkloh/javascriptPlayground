c56e64d8945aaaf645a9685cc16ef228f5fa5b22
LK Loh <lkloh2410@gmail.com>
2017-09-12 10:08:34 -0700

    Promise obj

--- promises.js
+++ promises.js
@@ -2,16 +2,19 @@
 // https://www.npmjs.com/package/node-fetch
 var fetch = require('node-fetch');
 
-fetch('https://api.github.com/users/lkloh')
-    .then((response) => {
-        return response.json();
-    }).then((userProfile) => {
-    	const today = new Date();
-    	const thisYear = today.getFullYear();
-        const timeJoined = new Date(userProfile['created_at']);
-        const yearJoined = timeJoined.getFullYear();
-        return thisYear - yearJoined + 1;
-    }).then((lengthOfUsage) => {
-    	console.log(lengthOfUsage);
-    });
+const yearsIUsedGithubFor = 
+	fetch('https://api.github.com/users/lkloh')
+	    .then((response) => {
+	        return response.json();
+	    }).then((userProfile) => {
+	    	const today = new Date();
+	    	const thisYear = today.getFullYear();
+	        const timeJoined = new Date(userProfile['created_at']);
+	        const yearJoined = timeJoined.getFullYear();
+	        return thisYear - yearJoined + 1;
+	    }).then((lengthOfUsage) => {
+	    	return lengthOfUsage;
+	    });
+
+console.log(yearsIUsedGithubFor);
 
