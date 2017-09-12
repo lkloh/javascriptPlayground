10472b5d7d7bed4aeb94d63cff44317b92a7ebc4
LK Loh <lkloh2410@gmail.com>
2017-09-12 10:07:33 -0700

    Tried date parsing

---
+++ promises.js
@@ -0,0 +1,17 @@
+
+// https://www.npmjs.com/package/node-fetch
+var fetch = require('node-fetch');
+
+fetch('https://api.github.com/users/lkloh')
+    .then((response) => {
+        return response.json();
+    }).then((userProfile) => {
+    	const today = new Date();
+    	const thisYear = today.getFullYear();
+        const timeJoined = new Date(userProfile['created_at']);
+        const yearJoined = timeJoined.getFullYear();
+        return thisYear - yearJoined + 1;
+    }).then((lengthOfUsage) => {
+    	console.log(lengthOfUsage);
+    });
+
