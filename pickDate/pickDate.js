cf02670be029c4ecfd66b0e7168e7e9e16e0ff7d
LK Loh <lkloh2410@gmail.com>
2017-11-10 09:13:02 -0800

    Next date

--- pickDate/pickDate.js
+++ pickDate/pickDate.js
@@ -1,7 +1,7 @@
 
 $(document).ready(function() {
 
-	function onlyNovember(date) {
+	function onlyNovember2017(date) {
 		if (date.getMonth() === 10) {
 			return [true, ''];
 		} else {
@@ -9,10 +9,16 @@ $(document).ready(function() {
 		}
 	}
 
-    $('#my_datepicker').datepicker({
+    $('#nov_2017_only').datepicker({
     	minDate: new Date(2017, 1, 1),
     	maxDate: new Date(2017, 12, 31),
-    	beforeShowDay: onlyNovember,
+    	dateFormat: 'dd-mm-yy',
+    	beforeShowDay: onlyNovember2017,
+    });
+
+    $('#show_two_months').datepicker({
+    	defaultDate: '+1m',
+    	numberOfMonths: 2,
     });
 
 });
