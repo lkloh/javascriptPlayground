62a5e872edf0b8a0b91ca4d3daa74ffe406a301d
LK Loh <lkloh2410@gmail.com>
2017-11-10 08:59:44 -0800

    Datepicker fun

--- pickDate/pickDate.js
+++ pickDate/pickDate.js
@@ -1,14 +1,18 @@
 
 $(document).ready(function() {
 
-	function notNovember(date) {
-		return true;
+	function onlyNovember(date) {
+		if (date.getMonth() === 10) {
+			return [true, ''];
+		} else {
+			return [false, '']
+		}
 	}
 
     $('#my_datepicker').datepicker({
     	minDate: new Date(2017, 1, 1),
     	maxDate: new Date(2017, 12, 31),
-    	//beforeShowDay: notNovember,
+    	beforeShowDay: onlyNovember,
     });
 
 });
