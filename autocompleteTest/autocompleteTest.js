1861fd9dae8830ece4dd098964e5f6fb164af85d
LK Loh <lkloh2410@gmail.com>
2017-11-23 15:22:58 -0800

    splice head and body

--- autocompleteTest/autocompleteTest.js
+++ autocompleteTest/autocompleteTest.js
@@ -26,14 +26,13 @@ $(document).ready(function() {
       "Scheme"
     ];
 
-    $('#normal_autocomplete').autocomplete({
-        source: availableTags
-    });
+    $('#normal_autocomplete').autocomplete({source: availableTags});
 
     var myIframe = document.getElementById('disfunctional_iframe').contentWindow.document;
     myIframe.open();
     myIframe.write('<html><head></head><body>Disfunctional: <input id="disfunctional_autocomplete"> </body></html>');
     $('#disfunctional_autocomplete').autocomplete({source: availableTags});
+    myIframe.close();
 
     var workingIframe = document.getElementById('working_iframe').contentWindow.document;
     workingIframe.open();
@@ -42,9 +41,19 @@ $(document).ready(function() {
         '<head>' + 
             '<script src="./external/jquery/jquery.js"></script>' +
             '<script src="./jquery-ui.min.js"></script>' +
+            '<script>' +
+                'console.log("YOLO")' +
+                '$("working_autocomplete").css("color", "red");' +
+            '</script>' +
         '</head>' +
         '<body> Working: <input id="working_autocomplete"> </body>' + 
     '</html>');
+    workingIframe.close();
 
+    var $html = "<html><head id='myhead'><title>titleTest</title></head><body class='my_awesome_body'><a href='test0'>test01</a><a href='test1'>test02</a><a href='test2'>test03</a></body></html>";
+    var re_head = new RegExp("<head[^>]*>\s*((?:.|\n)*?)</head>", "i");
+    var re_body = new RegExp("<body[^>]*>(.*?|\n)<\/body>", "i");
+    console.log(re_head.exec($html)[1]);
+    console.log(re_body.exec($html)[1]);
 
 });
