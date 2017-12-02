8e274277d7842de62bb09e5323552ea7cbc3b271
LK Loh <lkloh2410@gmail.com>
2017-12-02 14:03:22 -0800

    parse nums

---
+++ parseNums/parseNums.js
@@ -0,0 +1,20 @@
+
+/* - '' -> null
+ +    // - null -> null
+ +    // - 0 -> 0
+ +    // - '0' -> 0
+ */
+const assert = require('assert');
+
+function myParsedNum(potentialNumber) {
+	var result = Number.parseInt(potentialNumber);
+	return Number.isNaN(result) ? null : result;
+}
+
+console.log('test');
+assert.equal(null, myParsedNum(''));
+assert.equal(null, myParsedNum(null));
+assert.equal(0, myParsedNum(0));
+assert.equal(0, myParsedNum('0'));
+assert.equal(17, myParsedNum('17'));
+assert.equal(17, myParsedNum(17));
--- undefinedTest.js
+++ undefinedTest.js
@@ -1,5 +1,8 @@
 
-var not_map = {foo: 'foo', bar: 'bar'};
+var not_map = {
+	foo: {a: 1, b: 2, c: 3}, 
+	bar: {a: 1, b: 2}
+};
 console.log(not_map);
 console.log(not_map.foo ? 'present' : 'not_present');
 console.log(not_map.not_present ? 'present' : 'not_present');
@@ -8,9 +11,10 @@ console.log(not_map.not_present ? 'present' : 'not_present');
 console.log();
 
 var map = new Map();
-map.set('foo', 'foo');
-map.set('bar', 'bar');
+map.set('foo', {a: 1, b: 2, c: 3});
+map.set('bar', {a: 1, b: 2});
 console.log(map);
 console.log(map.get('foo') ? 'present' : 'not_present');
+console.log(map.get('not_present') ? 'present' : 'not_present');
 console.log(map.get('not_present') ? 'present' : 'not_present');
 
