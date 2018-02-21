f5ce884ae96371b641f3fdd669909636c1ee78b6
LK Loh <lkloh2410@gmail.com>
2018-02-20 17:29:12 -0800

    sinon time and moment

--- sinonTest.js
+++ sinonTest.js
@@ -6,10 +6,10 @@ var fakeDate = new Date(`2017-12-14T12:00-08:00`).getTime();
 clock = sinon.useFakeTimers(fakeDate);
 new Date(); //=> return the fake Date 'Dec 14, 2017 at 12pm in UTC-08:00'
 
-console.log(`time now:`);
-console.log(moment.utc());
+console.log(`time now: ${moment.utc()}`);
 
 clock.restore();
 new Date(); //=> will return the real time again (now)
 
+console.log(`real time: ${moment.utc()}`);
 
