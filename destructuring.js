87b7ebbad4b344474ecad86ee29cbd2e0d4e838c
LK Loh <lkloh2410@gmail.com>
2018-03-17 14:18:24 -0700

    destructuring

--- destructuring.js
+++ destructuring.js
@@ -7,3 +7,11 @@ console.log(`x: ${x}, y: ${y}, z: ${z}`);
 let pp = {xx: 10, yy:20};
 let {zz, yy, xx} = pp;
 console.log(`xx: ${xx}, yy: ${yy}, zz: ${zz}`);
+
+
+function destructure({z=30, y=20, x=10}) {
+  console.log(`x: ${x}, y: ${y}, z: ${z}`);
+}
+
+destructure({x: 11, y: 22, z: 33}); // all arguments present
+destructure({x: 11, y: 22}); // one argument missing
