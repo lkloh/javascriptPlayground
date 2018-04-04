5a868199316182331ca57f93be3f340e223f13a6
LK Loh <lkloh2410@gmail.com>
2018-04-04 01:26:08 -0700

    print counts

---
+++ reduceCombos.js
@@ -0,0 +1,37 @@
+
+const d = {
+  'all': {
+    'students': {
+      'united_states': {
+        'seattle': 2345,
+        'pittsburgh': 456,
+        'san_francisco': 99999,
+        'new_york': 87780,
+      },
+      'canada': {
+        'toronto': 19,
+        'ottawa': 21,
+      },
+      'china': {
+        'beijing': 7,
+        'shanghai': 79,
+        'hunan': 12,
+      },
+    },
+    'faculty': {
+      'united_states': {
+        'seattle': 458,
+        'pittsburgh': 3463,
+        'chicago': 49,
+      },
+      'canada': {
+        'toronto': 49,
+        'waterloo': 23,
+      },
+    },
+  },
+};
+
+
+console.log(d);
+
