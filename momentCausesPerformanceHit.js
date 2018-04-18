1dadec666e42880522eb545ac7b033ba4e30c6f4
LK Loh <lkloh2410@gmail.com>
2018-04-18 13:17:33 -0700

    Date does not cause such a huge perf hit

--- momentCausesPerformanceHit.js
+++ momentCausesPerformanceHit.js
@@ -2,17 +2,27 @@ const moment = require(`moment`);
 const performance = require(`performance-now`);
 
 function formatUsingMoment(dateStr) {
+
   const date = moment(dateStr);
   return date.format(`YYYY-MM-DD`);
 }
 
-function measureWithOrWithout(useMoment) {
+function formatUsingDate(dateStr) {
+  const timestamp = Date.parse(dateStr);
+  if (isNaN(timestamp)) {
+    return dateStr;
+  }
+  const date = new Date(timestamp);
+  return date.toString(`MM-DD-YYYY`);
+}
+
+function measureWithOrWithout(func, useMoment) {
   const start = performance();
 
   let v = null;
   for (let i = 0; i < 10000; i++) {
     if (useMoment) {
-      v = formatUsingMoment(i % 2 === 0 ? `2009-05-25T12:34:59` : `2018-04-18T11:17:23`);
+      v = func(i % 2 === 0 ? `2009-05-25T12:34:59` : `2018-04-18T11:17:23`);
     } else {
       v = `Boring`;
     }
@@ -20,9 +30,11 @@ function measureWithOrWithout(useMoment) {
 
   const end = performance();
 
-  console.log(`time taken when useMoment is ${useMoment} is: ` + (end - start));
+  console.log(`time taken for ${func.name} when useMoment is ${useMoment} is: ` + (end - start));
 }
 
-measureWithOrWithout(true);
-measureWithOrWithout(false);
+measureWithOrWithout(formatUsingMoment, true);
+measureWithOrWithout(formatUsingMoment, false);
+measureWithOrWithout(formatUsingDate, true);
+measureWithOrWithout(formatUsingDate, false);
 
