b97eb689e8aac6cdc73a25a766346e450b3d9719
LK Loh <lkloh2410@gmail.com>
2018-06-05 13:30:18 -0700

    Update duration

--- convertSeconds.js
+++ convertSeconds.js
@@ -3,16 +3,16 @@
 // convert to properly formatted interval
 function convertSeconds(seconds) {
   if (seconds < 60) {
-    return seconds + ' s';
+    return seconds + 's';
   } else if (seconds < 60 * 60) {
     const numMinutes = Math.floor(seconds / 60);
-    return numMinutes + ' m, ' + convertSeconds(seconds % 60);
+    return numMinutes + 'm, ' + convertSeconds(seconds % 60);
   } else if (seconds < 24 * 60 * 60) {
     const numHours = Math.floor(seconds / (60 * 60));
-    return numHours + ' hrs, ' + convertSeconds(seconds % (60 * 60));
+    return numHours + 'hr, ' + convertSeconds(seconds % (60 * 60));
   } else {
     const numDays = Math.floor(seconds / (24 * 60 * 60));
-    return numDays + ' days, ' + convertSeconds(seconds % (24 * 60 * 60));
+    return numDays + 'd, ' + convertSeconds(seconds % (24 * 60 * 60));
   }
   return '';
 }
@@ -21,3 +21,4 @@ console.log(convertSeconds(59));
 console.log(convertSeconds(3500));
 console.log(convertSeconds(3601));
 console.log(convertSeconds(20000));
+console.log(convertSeconds(123456));
