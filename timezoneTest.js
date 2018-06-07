73ec2f2136aa668e4c71acb57520aa82474f8219
LK Loh <lkloh2410@gmail.com>
2018-06-06 19:38:29 -0700

    parse

--- timezoneTest.js
+++ timezoneTest.js
@@ -1,4 +1,7 @@
 const moment = require('moment');
 
-const d = moment.utc('2018-07-18T01:00:00+08:00').format('ddd, MMM D, YYYY');
-console.log(d);
+const utcDate = moment.utc('2018-07-18T23:00:00-08:00').format('ddd, MMM D, YYYY');
+console.log(`utcDate: ${utcDate}`);
+
+const date = moment('2018-07-18T23:00:00-08:00').format('ddd, MMM D, YYYY');
+console.log(`date: ${date}`);
